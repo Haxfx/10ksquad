c3c9b307dfe31d0dc0ef2470881044214114944d
Many <sebastiaan@hotflux.nl>
2025-02-04 23:07:12 +0100

    fix(cutenfts)

--- app/components/NftSection.tsx
+++ app/components/NftSection.tsx
@@ -9,11 +9,11 @@ const NftSection = () => {
           <div className="w-full lg:w-1/2">
             <h2 className="text-3xl sm:text-4xl lg:text-5xl mb-6 lg:w-[80%] text-pink tracking-wide-plus">
               <span className="font-luckiest">Cute </span>
-              <span className="font-adrip text-darkpink text-4xl">NFTs </span>
+              <span className="font-adrip text-darkpink text-5xl">NFTs </span>
               <span className="font-luckiest">For </span>
               <br className="hidden sm:block" />
               <span className="font-luckiest">Cute </span>
-              <span className="font-adrip text-darkpink text-4xl">Degens</span>
+              <span className="font-adrip text-darkpink text-5xl">Degens</span>
             </h2>
             <p className="text-xs sm:text-sm w-full lg:w-2/3 mb-8 text-black opacity-[0.5] leading-relaxed">
               The 10k Squad was created as a place for all the misfits. 
